c3a52b44af843a2afe39be9799da1a5b944e7fa6
explorall <00thogus@gmail.com>
2020-05-03 14:39:39 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,6 @@
 /* Copyright (C) 2020 Noel Nyback
  * Copyright (C) 2020 Ludwig Gustafsson
+ * Copyright (C) 2020 Gustav Thorén
  * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
