5142d4b80820b66950e76158e96f745260163020
alvajs100 <63055832+alvajs100@users.noreply.github.com>
2020-05-08 14:45:12 +0200

    ScrollableList

--- tutusClavis/ui.ino
+++ tutusClavis/ui.ino
@@ -1,22 +1,22 @@
 /* Copyright (C) 2020 Noel Nyback
- * Copyright (C) 2020 Ludwig Gustafsson
- * Copyright (C) 2020 Gustav Thorén
- * Copyright (C) 2020 Victor Engdahl
- * Copyright (C) 2020 Alva Johansson Staaf
- * 
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+   Copyright (C) 2020 Ludwig Gustafsson
+   Copyright (C) 2020 Gustav Thorén
+   Copyright (C) 2020 Victor Engdahl
+   Copyright (C) 2020 Alva Johansson Staaf
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 //If time permits we need some way to automatically log out due to inactivity (increased safety)
 
@@ -30,15 +30,15 @@ byte currentUser;
 char * newNameMenu()
 {
   //Calling Keyboard function to get the string
-//  displayPrint("Username:");
-//  userName = useAlphabet();
+  //  displayPrint("Username:");
+  //  userName = useAlphabet();
   //Confirming name to move on
 }
 
 //Returns an array with the accessible keys
 int * keyAccessMenu()
 {
-  
+
 }
 
 //Shows a message on what is missing from the user if something is missing
@@ -50,26 +50,53 @@ bool finishMenu(char Name[9], word pin, int access[8])
 
 //Takes option "headlines" as parameters and returns which option chosen (∗ returns 0)
 //Displays and handles the scrolling and chosing in a list of size options
-byte scrollableList(/*list of option headlines, */byte options)
+byte scrollableList(String headlines[], byte options)
 {
-  
+  for (int i = 0; i < options; i++) {
+    String s1;
+    String s2;
+    s1 = ">" + headlines[i];                                //Gjorde istället så att man scrollar och väljer enter med *. Returnerar alltså 0-längden på headlines.
+    s2 = headlines[i + 1];
+    if (i == options - 1) {
+      s2 = " ";
+    }
+
+    displayPrint(s1, 0, 0);
+    displayPrint(s2, 0, 1);
+
+    while (true) {
+      char c = keypadInput();
+      if (c == '5') {
+        //Serial.println("Inne i =5");
+        break;
+      }
+      else if (c == '*') {
+        //Serial.println("Inne i =*");
+        displayClear();
+        return i;
+      }
+    }
+  }
 }
+
 
 //Displays available keys##########
 void keysMenu()
 {
   byte choosenKey;
-  String line1="Choose  x x x x ";
-  String line2="a key  x x x x  ";
-  
+  String line1 = "Choose  x x x x ";
+  String line2 = "a key  x x x x  ";
+
   bool Return;
-  
+
   while (true)
   {
     //Prints available keys on the screen
-    
-    Return=moveToKey(choosenKey, currentUser);
-    if (Return){return;}
+
+    Return = moveToKey(choosenKey, currentUser);
+    if (Return) {
+      return;
+    }
   }
 }
 
@@ -86,32 +113,34 @@ void addUserMenu()
   char Name[9];
   word pin;
   byte access;
-  bool Return=false;
+  bool Return = false;
   byte choice;
-  
+
   while (true)
   {
-    choice=scrollableList(/*list of available options,*/ 4);
-    switch(choice)
+    choice = scrollableList(/*list of available options,*/ 4);
+    switch (choice)
     {
       case 1:
         strcpy(Name, newNameMenu());
         break;
       case 2:
-        pin=newPinMenu();
+        pin = newPinMenu();
         break;
       case 3:
-        access=keyAccessMenu();
+        access = keyAccessMenu();
         break;
       case 4:
-        Return=finishMenu(Name, pin, access);
+        Return = finishMenu(Name, pin, access);
         break;
       default:
-      //If chosen option 0 return to 'manageUsersMenu'
-        Return=true;
+        //If chosen option 0 return to 'manageUsersMenu'
+        Return = true;
         break;
     }
-    if (Return){return;}
+    if (Return) {
+      return;
+    }
   }
 }
 
@@ -137,8 +166,8 @@ void manageUsersMenu()
   byte choice;
   while (true)
   {
-    choice=scrollableList(/*list of available options,*/ 3);
-    switch(choice)
+    choice = scrollableList(/*list of available options,*/ 3);
+    switch (choice)
     {
       case 1:
         addUserMenu();
@@ -166,17 +195,17 @@ bool logOut()
 void mainMenu()
 {
   byte choice;
-  byte options=3;
+  byte options = 3;
 
-  if (userAdmin(currentUser)==255)
+  if (userAdmin(currentUser) == 255)
   {
     options++;
   }
-  
+
   while (true)
   {
-    choice=scrollableList(/*list of available options,*/ options);
-    switch(choice)
+    choice = scrollableList(/*list of available options,*/ options);
+    switch (choice)
     {
       case 1:
         keysMenu();
@@ -199,9 +228,11 @@ void mainMenu()
 }
 
 //The highest function in the ui hierarchy which handles logging in
-void logIn(bool firstStartup=false)
+void logIn(bool firstStartup = false)
 {
-  if (firstStartup==true){mainMenu();}
+  if (firstStartup == true) {
+    mainMenu();
+  }
   while (true)
   {
     //Log in handling
@@ -213,7 +244,7 @@ void logIn(bool firstStartup=false)
 void firstStartup()
 {
   //Calls 'newNameMenu' and 'newPinMenu'
-  
+
   logIn(true);
 }
 
