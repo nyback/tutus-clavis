026e221320cb41fa03bcc598d402f879d19da1d6
Noel Nyback <noel.nyback@gmail.com>
2020-05-08 11:33:58 +0200

    Fixed user delete.

--- tutusClavis/user.ino
+++ tutusClavis/user.ino
@@ -77,6 +77,7 @@ void userDelete(byte id)
   // Deletes user from array and subs one from userNumber.
   for (byte i = id; i < userNumber; i++) {
     users[i] = users[i+1];
+    users[i].id--;
 
     // Clears info from last user.
     users[i+1].id = 0;
