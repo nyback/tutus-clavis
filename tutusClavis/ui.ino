151611054035ab3287237620dd6c9f93a7f571b7
Noel Nyback <noel.nyback@gmail.com>
2020-05-09 23:03:58 +0200

    Fixed new pin finc.

--- tutusClavis/ui.ino
+++ tutusClavis/ui.ino
@@ -352,8 +352,8 @@ word newPinMenu() {
     int passI = pass.toInt();
     word passW = (word) passI;
 
-    userSetPass(currentUser, passW);
-    userSave();
+//    userSetPass(currentUser, passW);
+//    userSave();
   return passI;
 }
 
