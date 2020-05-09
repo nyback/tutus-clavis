3518d16bb7f10e154d59bbd0e9542f5d39f9cdf0
victorengdahl <46004727+victorengdahl@users.noreply.github.com>
2020-05-10 00:38:53 +0200

    Update ui.ino

--- tutusClavis/ui.ino
+++ tutusClavis/ui.ino
@@ -576,40 +576,65 @@ void mainMenu() {
   while (true)
   {
     choice = scrollableList(headlines, options);
-    switch (choice)
-    {
-      case 1:
-        keysMenu();
-        break;
-      case 2:
-        displayClear();
-        displayPrint("Logging off",0,0);
-        delay(1500);
-        return;   
-      case 3:
-        //delay(5000);
-        word pass = newPinMenu();
+    Serial.print("choice: ");
+    Serial.println(choice);
+//    switch (choice)
+//    {
+//      case 1:
+//        keysMenu();
+//        break;
+//      case 2:
+//        displayClear();
+//        displayPrint("Logging off",0,0);
+//        delay(1500);
+//        return;   
+//      case 3:
+//        //delay(5000);
+//        word pass = newPinMenu();
+//
+//        delay(1000);
+//        userSetPass(currentUser, pass);
+//        userSave();
+//        break;
+//      case 4:
+//        Serial.println("before");
+//        manageUsersMenu();
+//        delay(1000);
+//        Serial.println("efter");
+//        break;
+//      default:
+//        //If chosen option is 2 or 0 it is interpretted as log out
+//        if (logOut()){
+//          return;
+//        }
+//        break;
+//        
+//    }
 
-        delay(1000);
-        userSetPass(currentUser, pass);
-        userSave();
-        break;
-      case 4:
-        //Only possible if the logged in account is an admin account (Excactly how remains to be figured out)
-        delay(1000);
-        Serial.println("före");
-        manageUsersMenu();
-        delay(1000);
-        Serial.println("efter");
-        break;
-      default:
-        //If chosen option is 2 or 0 it is interpretted as log out
-        if (logOut()){
+    if (choice == 1){
+      keysMenu();
+    } else if (choice == 2){
+      displayClear();
+      displayPrint("Logging off",0,0);
+      delay(1500);
+      return;  
+    } else if (choice == 3){
+       word pass = newPinMenu();
+
+       delay(1000);
+       userSetPass(currentUser, pass);
+       userSave();
+    } else if (choice == 4){
+      Serial.println("before");
+       manageUsersMenu();
+       delay(1000);
+       Serial.println("efter");
+    } else {
+      if (logOut()){
           return;
         }
-        break;
-        
     }
+
   }
 }
 
